719e9c6ded0f90968add0e9268ed569fd3ad6163
EngiSon <lleopardchaun@gmail.com>
2022-04-24 13:38:56 +0200

    Cards now properly render when added, other than the date, which isn't visible

--- todo-client/src/App.js
+++ todo-client/src/App.js
@@ -1,25 +1,9 @@
 import { Stack } from '@mui/material';
+import { useState } from 'react';
 import './App.css';
 import InputRow from './components/InputRow';
 import TodoCard from './components/TodoCard';
 import ToggleButtons from './components/ToggleButtons';
-
-function App() {
-  return (
-    <div id='app-container'>
-      <h2 id='centered'>Todo app</h2>
-      <div id='centered'>
-        <InputRow id='input-row' addTodo={addTodo}/>
-      </div>
-      <div id='centered-topmargin'>
-        <ToggleButtons/>
-      </div>
-      <Stack>
-        {todos}
-      </Stack>
-    </div>
-  );
-}
 
 class Todo {
   constructor(name, description, dueDate, status) {
@@ -30,10 +14,38 @@ class Todo {
   }
 }
 
-const todos = [];
+function App() {
+  const [todos, setTodos] = useState([]);
+  
+  const todosList = todos.map(todo => (
+    <TodoCard
+      name = {todo.name}
+      desc = {todo.desc}
+      date = {todo.date}
+    />
+  ))
 
-function addTodo(name, desc, date) {
-  todos.push(new Todo(name, desc, date, 'prog'));
+  function addTodo(name, desc, date) {
+    const newTodo = new Todo(name, desc, date, 'prog');
+    setTodos([...todos, newTodo])
+  }
+
+  return (
+    <div id='app-container'>
+      <h2 id='centered'>Todo app</h2>
+      <div id='centered'>
+        <InputRow id='input-row' addTodo={addTodo}/>
+      </div>
+      <div id='centered-topmargin'>
+        <ToggleButtons/>
+      </div>
+      <div id='centered'>
+        <Stack>
+          {todosList}
+        </Stack>
+      </div>
+    </div>
+  );
 }
 
 export default App;
