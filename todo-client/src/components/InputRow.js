8d7d9c410a2e59f130b11f289a127ac3cf800e7c
EngiSon <lleopardchaun@gmail.com>
2022-04-24 11:13:27 +0200

    Added hooks to InputRow. InputRow now correctly clears input values when submit

--- todo-client/src/components/InputRow.js
+++ todo-client/src/components/InputRow.js
@@ -1,16 +1,33 @@
 import * as React from 'react';
+import {useState} from 'react';
 import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
 import { Card, IconButton, Stack } from '@mui/material';
 
 export default function InputRow(props) {
 
+  const [name, setName] = useState('');
+  const [description, setDesc] = useState('');
+  const [date, setDate] = useState('');
+
   function handleSubmit(e) {
     e.preventDefault();
-    const name = document.getElementById('name');
-    const desc = document.getElementById('description');
-    const date = document.getElementById('date');
-    props.addTodo(name, desc, date, 'prog')
+    props.addTodo(name, description, date, 'prog')
+    setName('');
+    setDesc('');
+    setDate('');
+  }
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleDescChange(e) {
+    setDesc(e.target.value);
+  }
+
+  function handleDateChange(e) {
+    setDate(e.target.value);
   }
 
   return (
@@ -20,16 +37,18 @@ export default function InputRow(props) {
           noValidate
           autoComplete="off"
         >
-          <TextField id="name" label="Name" variant="filled"/>
-          <TextField id="description" label="Todo" variant="filled"/>
+          <TextField id="name-input" label="Name" variant="filled" value={name} onChange={handleNameChange}/>
+          <TextField id="description-input" label="Todo" variant="filled" value={description} onChange={handleDescChange}/>
           <TextField
-            id="date"
+            id="date-input"
             label="Due date"
             type="date"
             variant='filled'
             InputLabelProps={{
               shrink: true,
             }}
+            value={date}
+            onChange={handleDateChange}
           />
         </Card>
         <IconButton color='primary' type='submit'>
