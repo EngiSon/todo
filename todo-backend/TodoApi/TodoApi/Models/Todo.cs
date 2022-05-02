11586ab4336646dd83e2f68fa5fe0141d9a51188
EngiSon <lleopardchaun@gmail.com>
2022-05-02 11:00:52 +0200

    Created backend
    Created and worked on backend for some time, will have to work on it more

---
+++ todo-backend/TodoApi/TodoApi/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿namespace TodoApi.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public Status Status { get; set; }
+    }
+
+    public enum Status
+    {
+        DONE,
+        PROG,
+        POSTPONED,
+        PENDING
+    }
+}
